048de2ba0ce10cad53f9ecd605bec67f465c8bac
JDawg123 <josephpinoy2@yahoo.com>
2023-02-22 19:33:05 -0600

    small JSX challenge

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-//What should be done?
-//"just tell me what to do , and i'll worry about how I get it done"
-ReactDOM.render(<h1 className="header">Declarative coding</h1>,document.querySelector("#root"))
 
-//How should it be done?
-//"Describe to me every step on how to do something, and I'll do it"
-let h1 = document.createElement("h1");
-const root = document.querySelector("#root")
-h1.textContent = 'imperative coding';
-h1.className = 'header'
-root.append(h1);
+const navBar = (
+  <nav>
+    <h1>Hocus Pocus</h1>
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+)
+
+ReactDOM.render(navBar,document.getElementById('root'))
+//render(what do i want to render, where to render it)
