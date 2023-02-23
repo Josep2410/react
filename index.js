476e4c6952d8298134f9a63a139f4b1200dd3c00
JDawg123 <josephpinoy2@yahoo.com>
2023-02-22 20:05:49 -0600

    Update React 18

--- index.js
+++ index.js
@@ -11,8 +11,13 @@ const navBar = (
 )
 
 //React 17
-ReactDOM.render(navBar,document.getElementById('root'))
+//ReactDOM.render(navBar,document.getElementById('root'))
 //render(what do i want to render, where to render it)
 
 //React 18 
-ReactDOM.createRoot(document.getElementById('root')).render(navBar);
+//ReactDOM.createRoot(document.getElementById('root')).render(navBar);
+
+//OR
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+.render(navBar);
