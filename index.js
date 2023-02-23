5597de3e64962aee241ce5154ecfb7f1dc7991ea
JDawg123 <josephpinoy2@yahoo.com>
2023-02-22 19:05:15 -0600

    declarative vs imperative

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-function Element(){
-  return (<p>Hello From the other side</p>)
-}
-function SayHB(){
-  return <h1>Happy Birthday</h1>
-}
-ReactDOM.render(
- <div>
-   <Element />
-   <SayHB />
- </div>,
-  document.querySelector("#root"))
+//What should be done?
+//"just tell me what to do , and i'll worry about how I get it done"
+ReactDOM.render(<h1 className="header">Declarative coding</h1>,document.querySelector("#root"))
+
+//How should it be done?
+//"Describe to me every step on how to do something, and I'll do it"
+let h1 = document.createElement("h1");
+const root = document.querySelector("#root")
+h1.textContent = 'imperative coding';
+h1.className = 'header'
+root.append(h1);
