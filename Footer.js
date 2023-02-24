499e651f4736a5897e65b6b8d922b6d9c389f424
JDawg123 <josephpinoy2@yahoo.com>
2023-02-24 14:47:37 -0600

    organize components

---
+++ Footer.js
@@ -0,0 +1,9 @@
+
+
+export default function Footer() {
+  return (
+      <footer className="myFooter">
+          <small>© 2023 Moreno development. All rights reserved.</small>
+      </footer>
+  )
+}
